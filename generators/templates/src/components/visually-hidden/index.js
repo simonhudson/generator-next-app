5573ccf758cafa2d48a9ed092a2c63101acd35c8
Hudson <Simon.Hudson@experian.com>
2020-05-10 13:45:40 +0100

    hidden

--- generators/templates/src/components/visually-hidden/index.js
+++ generators/templates/src/components/visually-hidden/index.js
@@ -7,14 +7,14 @@ const VisuallyHidden = (props) => {
 	const element = props.element ? props.element.toLowerCase() : 'span';
 
 	const StyledVisuallyHidden = styled[element]`
-        border: 0;
-        clip: rect(0 0 0 0);
-        height: 0;
-        margin: 0;
-        overflow: hidden;
-        padding: 0;
-        position: absolute;
-        width: 0;
+		border: 0;
+		clip: rect(0 0 0 0);
+		height: 0;
+		margin: 0;
+		overflow: hidden;
+		padding: 0;
+		position: absolute;
+		width: 0;
 	`;
 
 	return <StyledVisuallyHidden>{props.children}</StyledVisuallyHidden>;
